31fb7b00f49b8e1496558f2a844de408d9a9ab75
zhu-s <2317709047@qq.com>
2021-01-23 21:29:57 +0800

    demo

---
+++ src/Assignment5/StuParser.java
@@ -0,0 +1,10 @@
+// package Assignment5;
+
+// public class StuParser {
+//     public static Student parseStringToStudent(String lineToParse){
+//         String[] strArr = lineToParse.split("\\|");
+//         if(strArr[0]=="Graduate"){
+
+//         }
+//     }
+// }
